b3f318c1eb48c47dff0955da0dce3690b1a3e91c
unknown <851938984@qq.com>
2019-10-13 00:09:09 +0800

    5

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,9 @@
 /*
  * @Descripttion: 
  * @Author: lvjing
- * @Date: 2019-10-12 20:33:07
+ * @Date: 2019-10-12 21:59:24
  * @LastEditors: lvjing
- * @LastEditTime: 2019-10-12 20:33:07
+ * @LastEditTime: 2019-10-13 00:08:08
  */
 'use strict';
 
@@ -11,12 +11,12 @@ const output = process.env.BABEL_OUTPUT;
 const modules = output == null ? false : output;
 
 const options = {
-  presets: [
-    ['@babel/env', { loose: true, modules }], '@babel/react'
-  ],
-  plugins: [
-    '@babel/proposal-object-rest-spread', ['@babel/proposal-class-properties', { loose: true }]
-  ],
+    presets: [
+        ['@babel/env', { loose: true, modules }], '@babel/react'
+    ],
+    plugins: [
+        '@babel/proposal-object-rest-spread', ['@babel/proposal-class-properties', { loose: true }]
+    ],
 };
 
-module.exports = options;
+export default options;
