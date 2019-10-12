901d987f70d31eb9274c904dc8a7819d46863492
unknown <851938984@qq.com>
2019-10-12 22:53:12 +0800

    fix(packjson.js)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@
  * @Author: lvjing
  * @Date: 2019-10-12 20:11:46
  * @LastEditors: lvjing
- * @LastEditTime: 2019-10-12 22:32:09
+ * @LastEditTime: 2019-10-12 22:50:44
  */
 
 import React, { Component } from 'react';
 
 import { Button } from './components'
+
 
 export default class App extends Component {
     render() {
