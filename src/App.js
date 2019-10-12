f0253038c16ee927682c440a9ed8435a01a49bcc
unknown <851938984@qq.com>
2019-10-12 23:19:19 +0800

    4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
  * @Author: lvjing
  * @Date: 2019-10-12 20:11:46
  * @LastEditors: lvjing
- * @LastEditTime: 2019-10-12 22:50:44
+ * @LastEditTime: 2019-10-12 23:12:04
  */
 
 import React, { Component } from 'react';
 
-import { Button } from './components'
+// import { Button } from './components'
 
+import { Button } from 'sm-ui';
 
 export default class App extends Component {
     render() {
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,17 +3,17 @@
  * @Author: lvjing
  * @Date: 2019-10-12 20:17:52
  * @LastEditors: lvjing
- * @LastEditTime: 2019-10-12 20:44:26
+ * @LastEditTime: 2019-10-12 23:16:59
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './index.css';
 
 const Button = ({ text }) => <button className="btn">这是一个组件按钮{text}</button>
 
-Button.propTypes = {
-  text: PropTypes.any
-};
+// Button.propTypes = {
+//   text: PropTypes.string
+// };
 
 export default Button;
